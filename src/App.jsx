6906c07e8d5fd6850e726d81bd8370b220e50274
ventuscoder <shivankcode@gmail.com>
2023-03-05 17:28:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Search from "./search";
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="app">
       
     </div>
   )
