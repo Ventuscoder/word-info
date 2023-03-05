728974ec55717a0a07c822e59d14b813dbf51468
ventuscoder <shivankcode@gmail.com>
2023-03-05 17:56:35 +0530

    Comment and format search.jsx

--- src/search.jsx
+++ src/search.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import './search.css';
 
 const Search = (props) => {
+    // handleSubmit function to pass data from child to parent
     const {handleSubmit} = props;
+    
     const [search, setSearch] = useState('');
 
+    // Function to handle change in input box - updating the state
     const handleChange = (e) => {
         setSearch(e.target.value);
     }
