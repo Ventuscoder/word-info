13e2d37a200ec5fde667cc45964229902d892808
ventuscoder <shivankcode@gmail.com>
2023-03-05 17:55:13 +0530

    Finish basic structure of Search component

--- src/search.jsx
+++ src/search.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './search.css';
 
-const Search = () => {
-    
+const Search = (props) => {
+    const {handleSubmit} = props;
+    const [search, setSearch] = useState('');
+
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    }
 
     return (
         <div className='search'>
-
+            <form className='form' onSubmit={(e)=>handleSubmit(e, search)}>
+                <input type="text" className='search-input' name="search" id="search" value={search} onChange={handleChange} placeholder='Type in a word...' />
+                <button type="submit" className='search-button' disabled={search.trim()===''}>Enter</button>
+            </form>
         </div>
     )
 }
