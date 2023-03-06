f20f8e489ef6fdf85a6f98f86b6d2269a68a7924
ventuscoder <shivankcode@gmail.com>
2023-03-06 20:46:18 +0530

    Create Meaning component

---
+++ src/meaning.jsx
@@ -0,0 +1,13 @@
+import './meaning.css';
+
+const Meaning = (props) => {
+    const {word, meanings} = props;
+
+    return (
+        <div className='meaning d-flex justify-content-center'>
+            <div></div>
+        </div>
+    )
+}
+
+export default Meaning;
