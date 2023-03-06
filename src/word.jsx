c1688e9af94b040006432d1e2cccc2ccff36c367
ventuscoder <shivankcode@gmail.com>
2023-03-06 19:20:24 +0530

    Create Word component

---
+++ src/word.jsx
@@ -0,0 +1,13 @@
+import './word.css';
+
+const Word = (props) => {
+    const {wordDescription} = props;
+
+    return (
+        <div>
+            wordDescription.phonetic;
+        </div>
+    )
+}
+
+export default Word;
