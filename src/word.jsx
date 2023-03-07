69a96478208061a06c725de6f19b0a173aaff2b6
ventuscoder <shivankcode@gmail.com>
2023-03-07 20:12:30 +0530

    Update Word styling

--- src/word.jsx
+++ src/word.jsx
@@ -12,7 +12,7 @@ const Word = (props) => {
         <div className='word d-flex justify-content-center flex-column'>
             <div className='p-2 w'>{wordDescription.word}</div>
             {wordDescription.phonetic != '' ? 
-                <button onClick={playAudio} className='audio-btn'><i class="fa-solid fa-ear-listen"></i> Pronounce this word</button> : null
+                <button onClick={playAudio} className='p-2 audio-btn'><i class="fa-solid fa-ear-listen"></i> Pronounce this word</button> : null
             }
         </div>
     )
